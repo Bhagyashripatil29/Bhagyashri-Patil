93cadc8093b9646e9b8f4cbad405f84d62c7341b
Bhagyashri Patil <bhagyashrip441@gmail.com>
2022-07-14 17:25:34 +0530

    this is my first commit

---
+++ src/ConstructorStudy/construct.java
@@ -0,0 +1,17 @@
+package ConstructorStudy;
+
+public class construct {
+	
+	
+
+	private void add() {
+
+		System.out.println("Add method called");
+	}
+
+	public static void main(String[] args) {
+		construct c=new construct();
+		c.add();
+	}
+
+}
